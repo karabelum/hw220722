dca49e6922cadfb48772956eed430b38bd504091
karabelum <saturnday@yandex.ru>
2023-01-15 21:19:25 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw220722
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Uchastniki.howManyPeople("Введите кол-во участников: ");
+            List<Uchastniki> uchastniki = new List<Uchastniki>();
+            for (int i = 0; i < count; i++)
+            {
+                uchastniki.Add(new Uchastniki(i + 1));
+            }
+            void vyvod()
+            {
+                Console.WriteLine("Все участники: ");
+                foreach (var item in uchastniki)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            Console.WriteLine("[1] Добавить участника\n[2] Удалить участника\n[3]Изменить возраст\n[4]Изменить имя");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    uchastniki.Add(new Uchastniki(uchastniki.Count + 1)); vyvod(); 
+                    break;
+                case "2":
+                    uchastniki.RemoveAt(Uchastniki.howManyPeople("") - 1);
+                    vyvod(); 
+                    break;
+                case "3":
+                    {
+                        vyvod();
+                        Console.WriteLine("Введите ID участника: ");
+                        int age = int.Parse(Console.ReadLine()) - 1;
+                        Console.WriteLine("Введите новый возраст: ");
+                        uchastniki.ElementAt(age).Age = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        vyvod();
+                        break;
+                    }
+                case "4":
+                    {
+                        vyvod();
+                        Console.WriteLine("Введите ID участника: ");
+                        int name = int.Parse(Console.ReadLine()) - 1;
+                        Console.WriteLine("Введите новое Имя: ");
+                        uchastniki.ElementAt(name).Name = String.Format(Console.ReadLine());
+                        Console.WriteLine();
+                        vyvod();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine();
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Uchastniki.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace hw220722
+{
+    class Uchastniki
+    {
+        public Uchastniki (int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public Uchastniki(int id)
+        {
+            Id = id;
+            Name = vod("Введите Имя участника:  ");
+            Age = howManyPeople("Введите возраст участника:  ");
+        }
+        string vod(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+        public static int howManyPeople(string text)
+        {
+            try
+            {
+                Console.Write(text);
+                return int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("что то пошло не так");
+                return howManyPeople(text);
+            }
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"ID Участника: {Id} \nИмя: {Name}\nВозраст: {Age}\n";
+        }
+    }
+}
